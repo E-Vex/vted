7a671b516f0c501ba6863bcb7d82457f3f36e280
elia <raiedaleve@gmail.com>
2026-08-07 19:15:55 +0300

    feat(terminal): add EnableTerminalRawMode function
    Implement raw mode by disabling canonical mode, echo, signal
    generation, and other terminal processing flags, then applying
    the settings via tcsetattr() with error handling.

--- src/terminal.c
+++ src/terminal.c
@@ -18,6 +18,48 @@ void SaveTerminalCurrentSettings()
     }
 }
 
+void EnableTerminalRawMode()
+{
+
+    struct termios raw_terminal_settings = old_terminal_settings;
+
+    /* enable the raw mode settings */
+    raw_terminal_settings.c_iflag &= ~ICRNL;  // ==> disable carriage return
+    raw_terminal_settings.c_iflag &= ~IXON;   // ==> disable software flow control
+    raw_terminal_settings.c_iflag &= ~BRKINT; // ==> disable serial break condition
+    raw_terminal_settings.c_iflag &= ~INPCK;  // ==> disable parity checking
+    raw_terminal_settings.c_iflag &= ~ISTRIP; // ==> disable 7-bit ASCII received
+
+    raw_terminal_settings.c_oflag &= ~OPOST; // ==> disable output post-processing
+
+    raw_terminal_settings.c_cflag |= CS8; // ==>  // ==> set character size to 8 bits
+
+    raw_terminal_settings.c_lflag &= ~ICANON; // ==> disable canonical mode
+    raw_terminal_settings.c_lflag &= ~ECHO;   // ==> disable echoing characters
+    raw_terminal_settings.c_lflag &= ~IEXTEN; // ==> disable extended input processing
+    raw_terminal_settings.c_lflag &= ~ISIG;   // ==> disable signal generationse
+
+    /*enable polling mode*/
+    raw_terminal_settings.c_cc[VMIN] = 1;  // ==>  read() returns as soon as 1 char is available
+    raw_terminal_settings.c_cc[VTIME] = 0; // ==> no timeout
+
+    /* apply changes */
+    int tcsetattr_return;
+    tcsetattr_return = tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_terminal_settings);
+
+    if (tcsetattr_return == -1)
+    {
+        perror("tcsetattr");
+        exit(EXIT_FAILURE);
+    }
+
+    /*========================================*
+     *  READ  TERMIOS  MAN   TO  MORE   DEPTH *
+     *  READ OR THE DOCS IN OF terminal-modes *
+     *  IN   ->   vted/docs/terminal-modes.md *
+     *========================================*/
+}
+
 int main()
 {
     struct termios old_terminal_settings;
